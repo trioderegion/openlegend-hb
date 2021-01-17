2d99980a9b6f5635d8184f3c789532e8550d6f45
trioderegion <matt.haentschke@gmail.com>
2021-01-16 21:04:11 -0500

    better version of the power level selection -- now an array of bools

--- module/sheets/OLItemSheet.js
+++ module/sheets/OLItemSheet.js
@@ -30,35 +30,7 @@ export default class OLItemSheet extends ItemSheet {
         super.activateListeners(html);
         
         /** owner only listeners */
-        // @todo find incantation for "this.actor.owner"
-            html.find(".pl-select").click(this.onSelectPowerLevel.bind(this));
     }
     
     /** Listeners */
-    onSelectPowerLevel(event){
-        const dataset = event.currentTarget.dataset;
-        const checked = event.currentTarget.checked;
-        const powerLevel = parseInt(event.currentTarget.value);
-        const isPresent = this.object.data.data.power.includes(powerLevel);
-        
-        if(checked && isPresent){
-            /** do nothing, odds are a bad state... */
-            return;
-        }
-
-        let powerUpdate = this.object.data.data.power;
-        if (checked){
-            //ensure this power level is present in the array
-            powerUpdate.push(powerLevel);
-            powerUpdate.sort();
-            
-        } else {
-            /** ensure this power level is NOT present in the array (no need to resort) */
-             powerUpdate = powerUpdate.filter( level => level !== powerLevel);
-        }
-        console.log("openlegend | Power Level list update");
-        console.log(powerUpdate);
-        return this.object.update({"data.power": powerUpdate})
-    }
-
 }
